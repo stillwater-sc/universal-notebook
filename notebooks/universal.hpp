c64607ffb80384a2c6e7e206ed7cec46b4cff381
Matthias Möller <m.moller@tudelft.nl>
2021-03-22 13:35:40 +0100

    Update universal.hpp

--- notebooks/universal.hpp
+++ notebooks/universal.hpp
@@ -3,10 +3,6 @@
 // Set include paths
 #pragma cling add_include_path("/home/jovyan/universal/include/")
 
-// Include main header files
-#include "/home/jovyan/universal/include/number/fixpnt/fixpnt"
-#include "/home/jovyan/universal/include/number/posit/posit"
-
 // Custom mime type image::png
 #include <string>
 #include <fstream>
